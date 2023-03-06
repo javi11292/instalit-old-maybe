a77b4239a1c2d3417676c8eaa1b50ae7e0acc433
Javier Robles <javiroblesfernandez@gmail.com>
2023-03-06 01:10:23 +0100

    Remove pages

--- pages/api/test.ts
+++
@@ -1,5 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function test(req: NextApiRequest, res: NextApiResponse) {
-  res.send("OK");
-}
--- pages/api/user/session.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-import { withSessionRoute } from "utils/session";
-
-async function session(req: NextApiRequest, res: NextApiResponse) {
-  res.send(req.session.id ? req.session : null);
-}
-
-export default withSessionRoute(session);
