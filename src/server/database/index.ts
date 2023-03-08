c1abf0b1f2ca0616d065692ede4614d1b5a314cd
Javier Robles <javiroblesfernandez@gmail.com>
2023-03-08 01:42:25 +0100

    Fix mongodb user

--- src/server/database/index.ts
+++ src/server/database/index.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const URL = process.env.MONGODB_URL as string;
+const URL = process.env.MONGODB_URI as string;
 
 const client = new MongoClient(URL, {
   auth: {
