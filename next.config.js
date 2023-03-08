563742f4b8aeff73fdc9a85502809af44526a22d
Javier Robles <javiroblesfernandez@gmail.com>
2023-03-08 12:55:45 +0100

    Fix sw

--- next.config.js
+++ next.config.js
@@ -6,6 +6,44 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+const options = {
+  matchOptions: {
+    ignoreVary: true,
+    ignoreSearch: true,
+  },
+  plugins: [
+    {
+      cacheKeyWillBeUsed: ({ request }) => {
+        const rsc = request.headers.get("rsc");
+        const prefetch = request.headers.get("next-router-prefetch");
+        const stateTree = request.headers.get("next-router-state-tree");
+
+        return [rsc, prefetch, stateTree].reduce((acc, header) => {
+          return header ? `${acc}_${header}` : acc;
+        }, request.url);
+      },
+    },
+  ],
+};
+
+const sw = new GenerateSW({
+  skipWaiting: true,
+  include: [],
+  swDest: `${__dirname}/public/sw.js`,
+  runtimeCaching: [
+    {
+      urlPattern: ({ url }) => url.pathname.match(/^\/api/),
+      handler: "NetworkFirst",
+      options,
+    },
+    {
+      urlPattern: /.*/,
+      handler: "CacheFirst",
+      options,
+    },
+  ],
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compress: false,
@@ -18,22 +56,10 @@ const nextConfig = {
     unoptimized: true,
   },
 
-  webpack(config, { dev }) {
-    if (dev || process.env.ANALYZE) return config;
+  webpack(config, { dev, isServer }) {
+    if (dev || isServer || process.env.ANALYZE) return config;
 
-    config.plugins.push(
-      new GenerateSW({
-        skipWaiting: true,
-        include: [],
-        swDest: `${__dirname}/public/sw.js`,
-        runtimeCaching: [
-          {
-            urlPattern: /.*/,
-            handler: "NetworkFirst",
-          },
-        ],
-      })
-    );
+    config.plugins.push(sw);
 
     return config;
   },
