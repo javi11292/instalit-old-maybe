ce20584e6931e413ff47d2a0f7d27cb7c4623200
Javier Robles <javiroblesfernandez@gmail.com>
2023-03-08 02:03:42 +0100

    Test

--- src/server/database/index.ts
+++ src/server/database/index.ts
@@ -2,11 +2,6 @@ import { MongoClient } from "mongodb";
 
 const URL = process.env.MONGODB_URI as string;
 
-const client = new MongoClient(URL, {
-  auth: {
-    username: process.env.MONGODB_USERNAME,
-    password: process.env.MONGODB_PASSWORD,
-  },
-});
+const client = new MongoClient(URL);
 
 export const database = client.db("database");
