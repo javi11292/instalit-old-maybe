f4df54bebb7b8ccafb611490442247d3de08c924
Javier Robles <javiroblesfernandez@gmail.com>
2023-03-08 22:56:11 +0100

    Fix cache version

--- next.config.js
+++ next.config.js
@@ -6,52 +6,55 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-const options = {
-  matchOptions: {
-    ignoreVary: true,
-  },
-  plugins: [
-    {
-      cacheKeyWillBeUsed: ({ request }) => {
-        const rsc = request.headers.get("rsc");
-        const prefetch = request.headers.get("next-router-prefetch");
-        const stateTree = request.headers.get("next-router-state-tree");
+const getSW = (buildId) => {
+  const options = {
+    cacheName: buildId,
+    matchOptions: {
+      ignoreVary: true,
+    },
+    plugins: [
+      {
+        cacheKeyWillBeUsed: ({ request }) => {
+          const rsc = request.headers.get("rsc");
+          const prefetch = request.headers.get("next-router-prefetch");
+          const stateTree = request.headers.get("next-router-state-tree");
 
-        const { value } = [rsc, prefetch, stateTree].reduce(
-          (acc, header) => {
-            if (!header) return acc;
+          const { value } = [rsc, prefetch, stateTree].reduce(
+            (acc, header) => {
+              if (!header) return acc;
 
-            acc.value = `${acc.value}${acc.separator}${header}`;
-            acc.separator = "&";
+              acc.value = `${acc.value}${acc.separator}${header}`;
+              acc.separator = "&";
 
-            return acc;
-          },
-          { value: request.url.split("?")[0], separator: "?" }
-        );
+              return acc;
+            },
+            { value: request.url.split("?")[0], separator: "?" }
+          );
 
-        return value;
+          return value;
+        },
       },
-    },
-  ],
-};
+    ],
+  };
 
-const sw = new GenerateSW({
-  skipWaiting: true,
-  include: [],
-  swDest: `${__dirname}/public/sw.js`,
-  runtimeCaching: [
-    {
-      urlPattern: ({ url }) => url.pathname.match(/^\/api/),
-      handler: "NetworkFirst",
-      options,
-    },
-    {
-      urlPattern: /.*/,
-      handler: "CacheFirst",
-      options,
-    },
-  ],
-});
+  return new GenerateSW({
+    skipWaiting: true,
+    include: [],
+    swDest: `${__dirname}/public/sw.js`,
+    runtimeCaching: [
+      {
+        urlPattern: ({ url }) => url.pathname.match(/^\/api/),
+        handler: "NetworkFirst",
+        options,
+      },
+      {
+        urlPattern: /.*/,
+        handler: "CacheFirst",
+        options,
+      },
+    ],
+  });
+};
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -65,10 +68,11 @@ const nextConfig = {
     unoptimized: true,
   },
 
-  webpack(config, { dev, isServer }) {
+  webpack(config, { dev, isServer, buildId }) {
+    console.log(buildId);
     if (dev || isServer || process.env.ANALYZE) return config;
 
-    config.plugins.push(sw);
+    config.plugins.push(getSW(buildId));
 
     return config;
   },
